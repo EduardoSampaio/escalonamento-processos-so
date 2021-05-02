548557dfa87404f6e7a9485c96c717884252a9e3
Eduardo <edwardviana@gmail.com>
2021-05-02 00:52:51 -0300

    logica timeline

--- src/app/pages/escalonamento/escalonamento.component.ts
+++ src/app/pages/escalonamento/escalonamento.component.ts
@@ -2,7 +2,7 @@ import { Escalonamento } from './../../models/escalonamento.model';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-escalonamento',
@@ -45,11 +45,47 @@ export class EscalonamentoComponent implements OnInit {
       this.tempoAtual++;
     }
 
+    var element = document.getElementById("P2-14");
+    element.classList.add("preenchido");
+
+    var element = document.getElementById("P2-15");
+    element.classList.add("preenchido");
+
+    var element = document.getElementById("P2-16");
+    element.classList.add("preenchido");
+
+    var element = document.getElementById("P2-17");
+    element.classList.add("preenchido");
+
+    var element = document.getElementById("P3-18");
+    element.classList.add("preenchido");
+
+    var element = document.getElementById("P3-19");
+    element.classList.add("preenchido");
+
+    var element = document.getElementById("P3-20");
+    element.classList.add("preenchido");
+
+    var element = document.getElementById("P3-21");
+    element.classList.add("preenchido");
+
   }
   previous(): void {
     if (this.tempoAtual > 0) {
       this.tempoAtual--;
     }
+
+    var element = document.getElementById("P3-18");
+    element.classList.remove("preenchido");
+
+    var element = document.getElementById("P3-19");
+    element.classList.remove("preenchido");
+
+    var element = document.getElementById("P3-20");
+    element.classList.remove("preenchido");
+
+    var element = document.getElementById("P3-21");
+    element.classList.remove("preenchido");
   }
   reset(): void { console.log('reset'); }
   start(): void { console.log('start') }
